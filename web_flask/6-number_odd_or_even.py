a2d201b7240cc5e321ba6be59f19d3bf2757df73
Hatim Selmun <selmun13@gmail.com>
2024-02-15 17:02:15 +0100

    odd or even?

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Odd or even number?
+"""
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+@app.route("/hbnb/", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+@app.route("/c/<text>")
+def is_fun(text):
+    text = text.replace('_', ' ')
+    return f"C {text}"
+
+@app.route("/python/", defaults={ 'text': 'is cool'})
+@app.route("/python/<text>", strict_slashes=False)
+def is_cool(text):
+    text = text.replace('_', ' ')
+    return f"Python {text}"
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def is_number(n):
+    return f"{n} is a number"
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    return render_template('5-number.html')
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def odd_or_even(n):
+    return render_template('6-number_odd_or_even.html', n=n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
